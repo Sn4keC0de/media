5ce653ea74469407833630e9b493ef593771a68c
Sn4keC0de <kevindarenas@gmail.com>
2025-02-11 15:47:20 -0300

    Agregado de Php & borrado de 3D library

---
+++ menu.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="menuStyle.css">
+</head>
+<body>
+    <header>
+        <div id="header">
+            <h1 class="LogoText">M.E.D.I.A</h1>
+            <!-- <img id="LogoImg" src="./img/MEDIA.png" alt=""> -->
+
+            <nav class="navbar">
+                <div class="nav_items" id="_items">
+                    <a href="#Experiments">Subjects</a>
+                    <a data-value="???" class="secretoLink" href="secreto.html">???</a>
+                    <a href="files.html">Files</a>
+                </div>
+
+                <div class="nav_toggle" id="_toggle">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
